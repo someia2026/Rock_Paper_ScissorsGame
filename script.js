7d6c837245cd0843f26aabd3520f91c3daec1d34
Someia <someia.ouaziz@gmail.com>
2024-11-25 14:44:49 +0100

    lastupdate2

--- script.js
+++ script.js
@@ -18,9 +18,57 @@ function ChoixJoueur() {
 // paramètre: non
 //retour: le choix de l'ordinateur sous forme de chaîne de caractère
 
-function choixOrdi() {
+function ChoixOrdi() {
     Math.ceil(Math.random()*3)
 }
+//Rôle: convertir les chiffres en mot
+//paramètre : le nombre choisi par l'ordi
+//retourne: le mot
+//1=papier 2=Feuille 3=Pierre
+function conversion(nbr){
+    let choixO
 
+}
 
+//rôle: comparer le choix du joueur et le choix de l'ordi
+//paramètre: choix du joueur et choix de l'ordinateur
+//retour: le gagnant
 
+function compare(ChoixJoueur, ChoixOrdi){
+    let result;
+    if (ChoixJoueur === ChoixOrdi){
+        result="égalité"
+    }else if (
+        ChoixJoueur==="ciseaux"&&ChoixOrdi==="papier"||
+        ChoixJoueur==="papier"&&ChoixOrdi==="pierre"||
+        ChoixJoueur==="pierre"&&ChoixOrdi==="ciseaux"
+    ){
+        result="Vous avez gagné!"
+    }
+    else {
+        result="L'ordinateur gagne!"
+    }
+    return result;    
+}
+
+//rôle: Afficher les choix et le gagnant
+//paramètre: choix du joueur, choix de l'ordinateur, résultat
+//retour:
+
+function affiche(ChoixJoueur, ChoixOrdi, resultat) {
+    alert(`choix du joueur:${ChoixJoueur}//choix de l'ordinateur: ${choixOrdinateur}
+        {resultat}`)
+    
+}
+//role: lancer le jeu en lançant les fonctions dans le bon ordre
+//paramètre: non
+//retour:non
+
+function Joueur() {
+    ChoixJoueur();
+    ChoixOrdi();
+    conversion();
+    compare();
+    affiche();
+    
+}
